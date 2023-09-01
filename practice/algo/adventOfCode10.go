a490c2de2c9eabc5c74ae19ba3f3ee7bbef3ff01
Bruce Bigirwenkya <brucebigirwenkya@gmail.com>
2023-09-01 16:18:05 +0300

    Advent of Code Day 10

---
+++ practice/algo/adventOfCode10.go
@@ -0,0 +1,110 @@
+package algo
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	NoOP = 1
+	Add  = 2
+)
+
+func adventOfCode10(filename string) int {
+
+	file, err := os.Open("./input/" + filename)
+	if err != nil {
+		fmt.Println(err)
+		panic("Error opening file")
+	}
+
+	defer file.Close()
+
+	clockCycle := 1
+	signalStrength := 0
+	registerValue := 1
+	renderContent := make([][]string, 6)
+	for i := 0; i < 6; i++ {
+		for j := 0; j < 40; j++ {
+			renderContent[i] = append(renderContent[i], "░░")
+		}
+	}
+	lines := 0
+
+	checkCycles := []int{20, 60, 100, 140, 180, 220}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if len(checkCycles) == 0 {
+			break
+		}
+		line := scanner.Text()
+		lines++
+		lineSegments := strings.Split(line, " ")
+		if len(lineSegments) == 2 {
+			renderContent = fillRenderer(renderContent, clockCycle, registerValue)
+			renderContent = fillRenderer(renderContent, clockCycle+1, registerValue)
+
+			clockCycle += 2
+			num, err := strconv.Atoi(lineSegments[1])
+			if err != nil {
+				panic("Error converting string to int")
+			}
+
+			if clockCycle >= checkCycles[0] {
+				signalStrength = signalStrength + (registerValue * checkCycles[0])
+				checkCycles = checkCycles[1:]
+			}
+
+			registerValue += num
+		} else {
+			renderContent = fillRenderer(renderContent, clockCycle, registerValue)
+			clockCycle += 1
+
+			if clockCycle >= checkCycles[0] {
+				signalStrength = signalStrength + (registerValue * checkCycles[0])
+				checkCycles = checkCycles[1:]
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic("Error reading file")
+	}
+
+	fmt.Println("Total clock cycles: ", clockCycle)
+	renderToScreen(renderContent)
+
+	return signalStrength
+}
+
+func fillRenderer(renderContent [][]string, clockCycle int, registerValue int) [][]string {
+	renderVal := "░░"
+
+	tracker := clockCycle % 40
+	if tracker == registerValue || tracker == registerValue+1 || tracker == registerValue+2 {
+		renderVal = "██"
+	}
+
+	// fmt.Println("Clock cycle: ", clockCycle, "Register value: ", registerValue, "render val: ", renderVal)
+
+	if (clockCycle % 40) == 0 {
+		renderContent[(clockCycle/40)-1][39] = renderVal
+	} else {
+		renderContent[clockCycle/40][clockCycle%40] = renderVal
+	}
+
+	return renderContent
+}
+
+func renderToScreen(content [][]string) {
+	for _, row := range content {
+		for _, col := range row {
+			fmt.Print(col)
+		}
+		fmt.Println()
+	}
+}
