b6c251f097127745a541ba9f2f561743b6e9b23a
Bruce Bigirwenkya <brucebigirwenkya@gmail.com>
2025-04-30 18:01:51 +0300

    valid Anagram

---
+++ internal/practice/algo/validAnagram_test.go
@@ -0,0 +1,40 @@
+package algo
+
+import "testing"
+
+func Test_isAnagram(t *testing.T) {
+	type args struct {
+		s string
+		t string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "anagram",
+			args: args{
+				s: "anagram",
+				t: "nagaram",
+			},
+			want: true,
+		},
+		{
+			name: "not anagram",
+			args: args{
+				s: "rat",
+				t: "car",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isAnagram(tt.args.s, tt.args.t); got != tt.want {
+				t.Errorf("isAnagram() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
