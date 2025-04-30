6c6def8db1a53aaddc4668ebcd500ffd8b20d4e7
Bruce Bigirwenkya <brucebigirwenkya@gmail.com>
2025-04-30 18:10:12 +0300

    isomorphic strings

---
+++ internal/practice/algo/ismorphicStrings.go
@@ -0,0 +1,17 @@
+package algo
+
+func isIsomorphic(s string, t string) bool {
+	charIndexS := make(map[byte]int)
+	charIndexT := make(map[byte]int)
+
+	for i := range s {
+		if charIndexS[s[i]] != charIndexT[t[i]] {
+			return false
+		}
+
+		charIndexS[s[i]] = i
+		charIndexT[t[i]] = i
+	}
+
+	return true
+}
