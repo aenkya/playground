ce1b9890f46fddee1b44bbfad2b7d83082be918b
Bruce Bigirwenkya <brucebigirwenkya@gmail.com>
2025-04-30 18:01:51 +0300

    valid Anagram

---
+++ internal/practice/algo/validAnagram.go
@@ -0,0 +1,25 @@
+package algo
+
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+
+	memo := make(map[rune]int)
+
+	for _, v := range s {
+		memo[v]++
+	}
+
+	for _, v := range t {
+		memo[v]--
+	}
+
+	for _, v := range memo {
+		if v != 0 {
+			return false
+		}
+	}
+
+	return true
+}
