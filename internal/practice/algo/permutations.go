c1758944c850311ff333ebd844a8e67d6afdd63b
Bruce Bigirwenkya <brucebigirwenkya@gmail.com>
2025-09-09 12:15:41 +0300

    v2 of permutations

--- internal/practice/algo/permutations.go
+++ internal/practice/algo/permutations.go
@@ -2,21 +2,31 @@ package algo
 
 func permute(nums []int) [][]int {
 	perms := make([][]int, 0)
-	var backtrack func([]int)
-	backtrack = func(perm []int) {
+	var backtrack func()
+	perm := make([]int, 0, len(nums))
+	used := make([]bool, len(nums))
+
+	backtrack = func() {
 		if len(perm) == len(nums) {
-			perms = append(perms, perm)
+			perms = append(perms, append([]int{}, perm...))
 			return
 		}
 
-		for _, num := range nums {
-			if !contains(perm, num) {
-				perm2 := append(perm, num)
-				backtrack(perm2)
+		for i := range nums {
+			if used[i] {
+				continue
 			}
+
+			used[i] = true
+			perm = append(perm, nums[i])
+
+			backtrack()
+
+			perm = perm[:len(perm)-1]
+			used[i] = false
 		}
 	}
 
-	backtrack([]int{})
+	backtrack()
 	return perms
 }
